d95b934b63166b17b690f09b4397f06118f9a4d3
TTToh724christ <Timothy12christ>
2019-11-02 14:55:06 +0800

    new

---
+++ Tim10-26.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 15:00:05 2019
+
+@author: ASUS
+"""
+#r=[]
+with open('C:/Users/ASUS/Desktop/reviews.txt','r') as f:
+    for line in f:
+       r.append(line)
+print(r[0])
